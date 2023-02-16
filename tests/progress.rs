0ca144ca936d9a980fd6d5659a5544639e9853e2
Mason Ginter <mason@dagint.com>
2023-02-16 04:52:52 -0500

    Add abi test

--- tests/progress.rs
+++ tests/progress.rs
@@ -4,3 +4,9 @@ fn tests() {
     t.pass("tests/build-tests/only_modify_marked.rs");
     t.compile_fail("tests/build-tests/require_module_name.rs");
 }
+
+#[test]
+fn build_abi_types() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/build-tests/different_abis.rs");
+}
