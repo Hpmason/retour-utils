f2289d50651fdc6aa70cd0600e1dd6d5393ac030
Mason Ginter <mason@dagint.com>
2023-07-05 00:22:15 -0400

    Improve docs

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,9 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum Error {
+    /// Detour error encountered within the [`retour`] crate
     DetourError(retour::Error),
+    /// Module trying to be hooked is not currently loaded in memory
     ModuleNotLoaded,
 }
 
@@ -16,7 +18,7 @@ impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::DetourError(e) => f.write_fmt(format_args!("Detour Error: {e:?}")),
-            Error::ModuleNotLoaded => todo!(),
+            Error::ModuleNotLoaded => f.write_str("Module trying to be hooked is not currently loaded in memory"),
         }
     }
 }
